2ce1af16efb3eeae43bed942f97992f9ce8bd751
sandramelisa <sanchezfalcons@gmail.com>
2018-11-19 16:26:48 -0500

    avances en html y css

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Para relacionar y seleccionar n,un elemento de html es---->document.getElementById
+const botonCifrado=document.getElementById("bcifrar");
+botonCifrado.addEventListener("click",cifrar);
+
+//function cifrar(){
+  let simbolo
+}//
+
+const botonDescrifrado=document.getElementById("bdescrifrar");
