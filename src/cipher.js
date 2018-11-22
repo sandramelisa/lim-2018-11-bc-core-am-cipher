aeb42289709b1146a6251b4e640a5ea635227526
sandramelisa <sanchezfalcons@gmail.com>
2018-11-22 16:45:45 -0500

    cipherjs y nombre de var que pedian

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,31 @@
 window.cipher = {
 
+encode:(offset,string)=>{
+     let resultado="";
+    //Para recorrer cada letra del mensaje ingresado
+     for (let i = 0; i < string.length; i++) {
+      //charCodeAt--->Para obtener el codigo Ascii de una letra
+      //La formula es para obtener la nueva posicion de la palabra en codigo Ascii
+      let ascii1=(string.toUpperCase().charCodeAt(i)-65+offset)%26+65;
+      //fromCharCode--->Para obtener la letra que corresponde al codigo Ascii
+      let letra=String.fromCharCode(ascii1);
+      resultado=resultado+letra;
+     }
+      return resultado;
+   },
+
+decode:(offset,string)=>{
+      let resultado2="";
+     //Para recorrer cada letra del mensaje ingresado
+      for (let j = 0; j < string.length; j++) {
+       //charCodeAt--->Para obtener el codigo ascii de una letra
+       //La formula es para obtener la nueva posicion de la palabra en codigo Ascii
+       let ascii2=(string.toUpperCase().charCodeAt(j)-90-offset)%26+90;
+       //fromCharCode--->Para obtener la letra que corresponde al codigo Ascii
+       let letra2=String.fromCharCode(ascii2);
+       resultado2=resultado2+letra2;
+      }
+       return resultado2;
+    }
+
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 
-function encode(mensajeInicial,clave){
-   let resultado="";
-  //Para recorrer cada letra del mensaje ingresado
-   for (let i = 0; i < mensajeInicial.length; i++) {
-    //charCodeAt--->Para obtener el codigo Ascii de una letra
-    //La formula es para obtener la nueva posicion de la palabra en codigo Ascii
-    let ascii1=(mensajeInicial.toUpperCase().charCodeAt(i)-65+clave)%26+65;
-    //fromCharCode--->Para obtener la letra que corresponde al codigo Ascii
-    let letra=String.fromCharCode(ascii1);
-    resultado=resultado+letra;
-   }
-    return resultado;
- }
 
 //DOM llamas a los elementos
 //function()=--->
@@ -23,27 +10,14 @@ const botonCifrar=document.getElementById("bcifrar");
 //llama los argumentos dom
 const mensajeFinal=function(){
     const mensaje1=document.getElementById("mensajePlano").value;
-    const desplazamiento=parseInt(document.getElementById("offset").value);
+    const desplazamiento=parseInt(document.getElementById("clave").value);
     //meter los argumentos en la funcion que ya cree
-    const output = encode(mensaje1,desplazamiento);
+    const output = cipher.encode(desplazamiento,mensaje1);
     document.getElementById("resultadoC").value=output;
 }
 
 botonCifrar.addEventListener("click",mensajeFinal);
 
-function decode(mensajeInicial2,clave2){
-   let resultado2="";
-  //Para recorrer cada letra del mensaje ingresado
-   for (let j = 0; j < mensajeInicial2.length; j++) {
-    //charCodeAt--->Para obtener el codigo ascii de una letra
-    //La formula es para obtener la nueva posicion de la palabra en codigo Ascii
-    let ascii2=(mensajeInicial2.toUpperCase().charCodeAt(j)-90-clave2)%26+90;
-    //fromCharCode--->Para obtener la letra que corresponde al codigo Ascii
-    let letra2=String.fromCharCode(ascii2);
-    resultado2=resultado2+letra2;
-   }
-    return resultado2;
- }
 
 //DOM llamas a los elementos
 //function()=--->
@@ -55,9 +29,9 @@ const botonDescifrar=document.getElementById("bdescifrar");
 //llama los argumentos dom
 const mensajeFinal2=function(){
     const mensaje2=document.getElementById("mensajePlano").value;
-    const desplazamiento2=parseInt(document.getElementById("offset").value);
+    const desplazamiento2=parseInt(document.getElementById("clave").value);
     //meter los argumentos en la funcion que ya cree
-    const output2 = decode(mensaje2,desplazamiento2);
+    const output2 = cipher.decode(desplazamiento2,mensaje2);
     document.getElementById("resultadoC").value=output2;
 }
 
