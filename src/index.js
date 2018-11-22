fee87c6978944e108d593f047232fe8e94e0fa85
sandramelisa <sanchezfalcons@gmail.com>
2018-11-22 14:36:16 -0500

    descifrado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-
-
 function encode(mensajeInicial,clave){
    let resultado="";
   //Para recorrer cada letra del mensaje ingresado
@@ -32,3 +30,35 @@ const mensajeFinal=function(){
 }
 
 botonCifrar.addEventListener("click",mensajeFinal);
+
+function decode(mensajeInicial2,clave2){
+   let resultado2="";
+  //Para recorrer cada letra del mensaje ingresado
+   for (let j = 0; j < mensajeInicial2.length; j++) {
+    //charCodeAt--->Para obtener el codigo ascii de una letra
+    //La formula es para obtener la nueva posicion de la palabra en codigo Ascii
+    let ascii2=(mensajeInicial2.toUpperCase().charCodeAt(j)-90-clave2)%26+90;
+    //fromCharCode--->Para obtener la letra que corresponde al codigo Ascii
+    let letra2=String.fromCharCode(ascii2);
+    resultado2=resultado2+letra2;
+   }
+    return resultado2;
+ }
+
+//DOM llamas a los elementos
+//function()=--->
+
+//getElementById-->para relacionar las etiquetas del html con el JS
+
+// boton q desencadena el evento
+const botonDescifrar=document.getElementById("bdescifrar");
+//llama los argumentos dom
+const mensajeFinal2=function(){
+    const mensaje2=document.getElementById("mensajePlano").value;
+    const desplazamiento2=parseInt(document.getElementById("offset").value);
+    //meter los argumentos en la funcion que ya cree
+    const output2 = decode(mensaje2,desplazamiento2);
+    document.getElementById("resultadoC").value=output2;
+}
+
+botonDescifrar.addEventListener("click",mensajeFinal2);
